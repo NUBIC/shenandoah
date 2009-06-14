8b6abcf84f350298512c8b40a30a2f4e7d042afd
Rhett Sutphin <rhett@detailedbalance.net>
2009-06-14 15:11:30 -0500

    Add shell runner to Shenandoah::Runner.

--- lib/shenandoah/runner.rb
+++ lib/shenandoah/runner.rb
@@ -1,38 +1,68 @@
 require 'fileutils'
+require 'erb'
 
 module Shenandoah
   class Runner
-    include FileUtils
-    
     def initialize(locator, options = {})
       @locator = locator
       @quiet = options[:quiet]
     end
-    
+
     # Runs the specified specs.  Returns an array of the specs that passed.
     def run_console(specs)
-      cd @locator.spec_path do
+      FileUtils.cd @locator.spec_path do
         runner_cmd = rhino_command(shen_path_to('lib/shenandoah/javascript/console/runner.js'))
         args = [
           shen_path_to,
           @locator.main_path,
           @locator.spec_path
         ]
-        
-        return specs.select { |spec| 
-          system("#{runner_cmd} '#{args.join("' '")}' '#{spec.sub(%r{^#{@locator.spec_path}/}, '')}' #{@quiet ? '> /dev/null' : ''}") 
+
+        return specs.select { |spec|
+          system("#{runner_cmd} '#{args.join("' '")}' '#{spec.sub(%r{^#{@locator.spec_path}/}, '')}' #{@quiet ? '> /dev/null' : ''}")
         }
       end
     end
-    
+
+    def run_shell
+      create_shell_html
+      create_shell_js
+
+      rlwrap = `which rlwrap`.chomp
+      cmd = "#{rlwrap} #{rhino_command('-f', shell_js_path, '-f', '-')}"
+      $stderr.puts "Starting shell with #{cmd}"
+      system(cmd)
+    end
+
     private
-    
+
     def shen_path_to(file=nil)
       File.expand_path(File.dirname(__FILE__) + "/../../#{file}")
     end
-    
+    alias :shenandoah_root :shen_path_to
+
     def rhino_command(*args)
       "java -jar '#{shen_path_to 'lib/shenandoah/javascript/console/js.jar'}' -w -debug '#{args.join("' '")}'"
+    end
+
+    def shell_html_path
+      "#{@locator.tmp_path}/shell.html"
+    end
+
+    def shell_js_path
+      "#{@locator.tmp_path}/shell.js"
+    end
+
+    def create_shell_html
+      $stderr.puts "Creating shell.html in #{shell_html_path}"
+      File.open(shell_html_path, 'w') do |f|
+        f.write "<html><body></body></html>"
+      end
+    end
+
+    def create_shell_js
+      js = ERB.new(File.read(shen_path_to('lib/shenandoah/javascript/console/shell.js.erb'))).result(binding)
+      File.open(shell_js_path, 'w') { |f| f.write js }
     end
   end
 end
--- spec/shenandoah/runner_spec.rb
+++ spec/shenandoah/runner_spec.rb
@@ -1,20 +1,23 @@
 require File.expand_path(File.dirname(__FILE__) + "/../spec_helper")
+require 'erb'
+require 'open3'
 
 require 'shenandoah/locator'
 require 'shenandoah/runner'
 
 describe Shenandoah::Runner do
   include Shenandoah::Spec::Tmpfile
-  
+
   before do
     @locator = Shenandoah::DefaultLocator.new(
       :spec_path => "#{tmpdir}/spec",
       :main_path => "#{tmpdir}/main",
       :tmp_path  => "#{tmpdir}/tmp"
     )
+    %w(spec main tmp).each { |d| FileUtils.mkdir_p "#{tmpdir}/#{d}" }
     @runner = Shenandoah::Runner.new(@locator, :quiet => true)
   end
-  
+
   def add_good_spec
     @good = tmpfile "spec/good_spec.js", <<-JS
       Screw.Unit(function () {
@@ -29,7 +32,7 @@ describe Shenandoah::Runner do
       <html><body></body></html>
     HTML
   end
-  
+
   def add_bad_spec
     @bad = tmpfile "spec/bad_spec.js", <<-JS
       Screw.Unit(function () {
@@ -44,22 +47,64 @@ describe Shenandoah::Runner do
       <html><body></body></html>
     HTML
   end
-  
+
   describe "#run_console" do
     it "runs passing specs" do
       add_good_spec
       @runner.run_console([@good]).should == [@good]
     end
-    
+
     it "fails on bad specs" do
       add_bad_spec
       @runner.run_console([@bad]).should == []
     end
-    
+
     it "runs all specs" do
       add_good_spec
       add_bad_spec
       @runner.run_console([@bad, @good]).should == [@good]
+    end
+  end
+
+  describe "#run_shell" do
+    before do
+      script_contents = ERB.new(<<-RUBY).result(binding)
+        # This is a small script to invoke Runner#run_shell so that it's
+        # input and output can be controlled for testing
+        <% $LOAD_PATH.select { |p| p =~ /shenandoah/ }.each do |p| %>
+          $LOAD_PATH.unshift(<%= p.inspect %>)
+        <% end %>
+        require 'rubygems'
+        require 'shenandoah/runner'
+        require 'shenandoah/locator'
+
+        loc = Shenandoah::DefaultLocator.new(
+          :main_path => <%= @locator.main_path.inspect %>,
+          :spec_path => <%= @locator.spec_path.inspect %>,
+          :tmp_path => <%= @locator.tmp_path.inspect %>
+        )
+
+        Shenandoah::Runner.new(loc).run_shell
+      RUBY
+      tmpfile "spec_shell.rb", script_contents
+    end
+
+    def run_shell(jslines)
+      Open3.popen3("ruby #{tmpdir}/spec_shell.rb") do |stdin, stdout, stderr|
+        stdin.write jslines
+        stdin.close
+        return stdout.read, stderr.read
+      end
+    end
+
+    it "runs an interactive shell" do
+      out, err = run_shell("1 + 1 + ' foo';\n")
+      err.should =~ /2 foo/
+    end
+
+    it "prints a banner" do
+      out, err = run_shell("quit();\n")
+      out.should =~ / Rhino JavaScript Shell/
     end
   end
 end
