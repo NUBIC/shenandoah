2d9622a4b2d02def9b61705f9c6cdd95183133f5
Rhett Sutphin <rhett@detailedbalance.net>
2009-06-14 16:55:23 -0500

    Rake tasks for basic shenandoah actions: run the server, run the specs, run the shell

---
+++ lib/shenandoah/tasks.rb
@@ -0,0 +1,58 @@
+require 'rake'
+require 'shenandoah/runner'
+require 'shenandoah/server'
+
+module Shenandoah
+  class Tasks
+    attr_accessor :locator, :options, :runner
+    
+    def initialize(options = {})
+      @options = options
+      @locator =
+        if options[:locator]
+          options[:locator]
+        else
+          DefaultLocator.new(options)
+        end
+      @runner = Shenandoah::Runner.new(@locator)
+      create_serve_task
+      create_shell_task
+      create_run_task
+    end
+    
+    def run_specs
+      files = @locator.spec_files
+      if ENV['SHEN_SPEC']
+        files = files.select { |f| f =~ /#{ENV['SHEN_SPEC']}/ }
+      end
+      successes = @runner.run_console(files)
+      if (successes.size != files.size)
+        raise "Shenandoah specs failed!"
+      end
+    end
+    
+    protected
+    
+    def create_serve_task
+      task('shen:serve') do |t|
+        Shenandoah::Server.set :locator, @locator
+        if @options[:project_name]
+          Shenandoah::Server.set :project_name, @options[:project_name]
+        end
+        Shenandoah::Server.run!
+      end
+    end
+    
+    def create_shell_task
+      task('shen:shell') do |t|
+        @runner.run_shell
+      end
+    end
+    
+    def create_run_task
+      task('shen:spec') do |t|
+        run_specs
+      end
+    end
+  end
+end
---
+++ spec/shenandoah/tasks_spec.rb
@@ -0,0 +1,76 @@
+require File.expand_path(File.dirname(__FILE__) + '/../spec_helper')
+
+require 'shenandoah/tasks'
+require 'shenandoah/locator'
+
+describe Shenandoah::Tasks do
+  describe "init" do
+    it "uses a DefaultLocator by default" do
+      Shenandoah::Tasks.new.locator.class.should == Shenandoah::DefaultLocator
+    end
+
+    it "configures the default locator with the provided options" do
+      Shenandoah::Tasks.new(:main_path => 'foo').locator.main_path.should == 'foo'
+    end
+  end
+
+  after do
+    Rake::Task.clear
+  end
+
+  describe "tasks" do
+    before do
+      Shenandoah::Tasks.new
+    end
+
+    it "adds a shen:serve task" do
+      Rake::Task::task_defined?('shen:serve').should be_true
+    end
+
+    it "adds a shen:shell task" do
+      Rake::Task::task_defined?('shen:shell').should be_true
+    end
+
+    it "adds a shen:spec task" do
+      Rake::Task::task_defined?('shen:spec').should be_true
+    end
+  end
+
+  describe "running" do
+    before do
+      @locator = mock(Shenandoah::DefaultLocator)
+      @runner  = mock(Shenandoah::Runner)
+
+      @tasks = Shenandoah::Tasks.new
+      @tasks.locator = @locator
+      @tasks.runner  = @runner
+    end
+
+    after do
+      ENV.delete 'SHEN_SPEC'
+    end
+
+    it "runs all the specs without ENV['SHEN_SPEC']" do
+      @locator.should_receive(:spec_files).and_return(%w(a b c))
+      @runner.should_receive(:run_console).with(%w(a b c)).and_return(%w(a b c))
+
+      @tasks.run_specs
+    end
+
+    it "only runs the specs matching ENV['SHEN_SPEC']" do
+      ENV['SHEN_SPEC'] = "b"
+
+      @locator.should_receive(:spec_files).and_return(%w(ab bc cd))
+      @runner.should_receive(:run_console).with(%w(ab bc)).and_return(%w(ab bc))
+
+      @tasks.run_specs
+    end
+
+    it "throws an exception on failure" do
+      @locator.should_receive(:spec_files).and_return(%w(a b c))
+      @runner.should_receive(:run_console).with(%w(a b c)).and_return(%w(a b))
+
+      lambda { @tasks.run_specs }.should raise_error(/Shenandoah specs failed!/)
+    end
+  end
+end
