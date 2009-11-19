NUBIC/shenandoah

commit 26cffa593f5f7f5e669ed38ac1d5add3eb132c30

Enable generator execution using a rake task.  Intended for use outside of rails.  Closes #1.

lib/shenandoah/tasks.rb
@@ -2,6 +2,8 @@ require 'rake'
 require 'shenandoah/locator'
 require 'shenandoah/runner'
 require 'shenandoah/server'
+require 'rails_generator'
+require 'rails_generator/scripts/generate'
 
 module Shenandoah
   class Tasks
@@ -19,6 +21,7 @@ module Shenandoah
       create_serve_task
       create_shell_task
       create_run_task
+      create_gen_task
     end
     
     def run_specs(pattern=nil)
@@ -36,6 +39,26 @@ module Shenandoah
       if (successes.size != files.size)
         raise "Shenandoah specs failed!"
       end
+    end
+    
+    def generate_spec(name)
+      raise "Please specify a spec name.  E.g., shen:generate[foo]." unless name
+      ::Rails::Generator::Base::prepend_sources( 
+        ::Rails::Generator::PathSource.new(
+          :shenandoah, File.join(File.dirname(__FILE__), '../../rails_generators'))
+      )
+      # These branches are functionally equivalent. They change the logging
+      # that rails_generator emits to omit the WD when the target is under
+      # the WD.
+      ENV['SHEN_SPEC_PATH'], dest =
+        if @locator.spec_path =~ /^#{FileUtils.pwd}\/?/
+          [@locator.spec_path.sub(/^#{FileUtils.pwd}\/?/, ''), FileUtils.pwd]
+        else
+          [@locator.spec_path, '']
+        end
+      ::Rails::Generator::Scripts::Generate.new.run(
+        ['shen_spec', '-t', name], :destination => dest,
+        :quiet => Rake.application.options.quiet)
     end
     
     protected
@@ -70,6 +93,13 @@ module Shenandoah
       desc "Run the JavaScript specs"
       task('shen:spec', :pattern) do |t, args|
         run_specs args.pattern
+      end
+    end
+    
+    def create_gen_task
+      desc "Generate a skeleton spec.  Give the spec name as a task arg -- i.e. shen:generate[commands]."
+      task('shen:generate', :basename) do |t, args|
+        generate_spec args.basename
       end
     end
   end

rails_generators/shen_spec/shen_spec_generator.rb
@@ -5,7 +5,7 @@ module Shenandoah
   module Generators
     class ShenSpecGenerator < ::Rails::Generator::NamedBase
       def manifest
-        spec_path = Shenandoah::Rails::Locator.new.spec_path.sub %r{^#{RAILS_ROOT}/}, ''
+        spec_path = ENV['SHEN_SPEC_PATH'] || Shenandoah::Rails::Locator.new.spec_path.sub(%r{^#{RAILS_ROOT}/}, '')
         record do |m|
           m.directory "#{spec_path}/#{File.dirname(file_path)}"
           m.template 'javascript_spec.js.erb', "#{spec_path}/#{file_path}_spec.js"

spec/shenandoah/tasks_spec.rb
@@ -8,7 +8,7 @@ require 'open3'
 describe Shenandoah::Tasks do
   describe "init" do
     include Shenandoah::Spec::Tmpfile
-    
+
     it "uses a DefaultLocator by default" do
       Shenandoah::Tasks.new.locator.class.should == Shenandoah::DefaultLocator
     end
@@ -16,7 +16,7 @@ describe Shenandoah::Tasks do
     it "configures the default locator with the provided options" do
       Shenandoah::Tasks.new(:main_path => '/foo').locator.main_path.should == '/foo'
     end
-    
+
     it "uses an explictly provided locator, ignoring other options" do
       loc = Shenandoah::DefaultLocator.new(:main_path => '/bar')
       tasks = Shenandoah::Tasks.new(:locator => loc, :main_path => 'foo')
@@ -36,9 +36,9 @@ describe Shenandoah::Tasks do
     it "adds a shen:serve task" do
       Rake::Task::task_defined?('shen:serve').should be_true
     end
-    
+
     it "gives the shen:serve task a description" do
-      Rake::Task['shen:serve'].full_comment.should == 
+      Rake::Task['shen:serve'].full_comment.should ==
         "Start the in-browser JavaScript spec runner on http://localhost:4410/"
     end
 
@@ -47,7 +47,7 @@ describe Shenandoah::Tasks do
     end
 
     it "gives the shen:shell task a description" do
-      Rake::Task['shen:shell'].full_comment.should == 
+      Rake::Task['shen:shell'].full_comment.should ==
         "Start the Shenandoah interactive JavaScript shell"
     end
 
@@ -56,8 +56,17 @@ describe Shenandoah::Tasks do
     end
 
     it "gives the shen:spec task a description" do
-      Rake::Task['shen:spec'].full_comment.should == 
+      Rake::Task['shen:spec'].full_comment.should ==
         "Run the JavaScript specs"
+    end
+
+    it "adds a shen:generate task" do
+      Rake::Task::task_defined?('shen:generate').should be_true
+    end
+
+    it "gives the shen:generate task a description" do
+      Rake::Task['shen:generate'].full_comment.should ==
+        "Generate a skeleton spec.  Give the spec name as a task arg -- i.e. shen:generate[commands]."
     end
   end
 
@@ -105,7 +114,7 @@ describe Shenandoah::Tasks do
       lambda { @tasks.run_specs }.should raise_error(/Shenandoah specs failed!/)
     end
   end
-  
+
   describe "running for real" do
     include Shenandoah::Spec::Tmpfile
 
@@ -169,15 +178,15 @@ describe Shenandoah::Tasks do
       err.should == ""
       out.should =~ %r{1 test\(s\), 0 failure\(s\)}
     end
-    
+
     it "reports all results together" do
       pending
       create_passing_spec('a')
       create_passing_spec('b')
       create_passing_spec('c')
-      
+
       out, err = run_specs
-      
+
       err.should == ""
       out.should =~ %r{3 test\(s\), 0 failure\(s\)}
     end
@@ -190,7 +199,7 @@ describe Shenandoah::Tasks do
       err.should =~ /Shenandoah specs failed/
       out.should =~ %r{1 test\(s\), 1 failure\(s\)}
     end
-    
+
     it "reports all failures" do
       pending
       create_failing_spec('a')
@@ -202,18 +211,90 @@ describe Shenandoah::Tasks do
       err.should =~ /Shenandoah specs failed/
       out.should =~ %r{3 test\(s\), 2 failure\(s\)}
     end
-    
+
     it "only runs the specs matching the arg" do
       create_passing_spec('foo')
       create_passing_spec('bar')
       create_passing_spec('baz')
-      
+
       out, err = run_specs('shen:spec[ba]')
-      
+
       err.should == ""
       out.should_not =~ %r{Running foo_spec}
       out.should =~ %r{Running bar_spec}
       out.should =~ %r{Running baz_spec}
+    end
+  end
+
+  describe "generation" do
+    include Shenandoah::Spec::Tmpfile
+
+    before do
+      @quiet = Rake.application.options.quiet
+      Rake.application.options.quiet = true
+    end
+
+    after do
+      Rake.application.options.quiet = @quiet
+    end
+
+    describe "when spec path is under the working directory" do
+      before do
+        @specdir = tmpdir('specs')
+        @tasks = Shenandoah::Tasks.new(:spec_path => @specdir)
+      end
+
+      it "fails without a name" do
+        lambda { @tasks.generate_spec(nil) }.
+          should raise_error(/Please specify a spec name.  E.g., shen:generate\[foo\]./)
+      end
+
+      it "generates the spec file" do
+        @tasks.generate_spec('arb')
+        File.exist?("#{@specdir}/arb_spec.js").should be_true
+      end
+
+      it "generates the HTML fixture" do
+        @tasks.generate_spec('zamm')
+        File.exist?("#{@specdir}/zamm.html").should be_true
+      end
+
+      describe "with nested paths" do
+        before do
+          @tasks.generate_spec("frob/zazz")
+        end
+
+        it "generates the spec file" do
+          File.exist?("#{@specdir}/frob/zazz_spec.js").should be_true
+        end
+
+        it "generates the HTML fixture" do
+          File.exist?("#{@specdir}/frob/zazz.html").should be_true
+        end
+      end
+    end
+
+    describe "when spec_path is not a child of working directory" do
+      before do
+        @original_wd = FileUtils.pwd
+        @specdir = tmpdir('specs')
+        @tasks = Shenandoah::Tasks.new(:spec_path => @specdir)
+        FileUtils.cd tmpdir('other')
+
+        @tasks.generate_spec("zip/zoom")
+      end
+
+      after do
+        FileUtils.cd @original_wd
+      end
+
+      it "generates the spec file" do
+        File.exist?("#{@specdir}/zip/zoom_spec.js").should be_true
+      end
+
+      it "generates the HTML fixture" do
+        File.exist?("#{@specdir}/zip/zoom.html").should be_true
+      end
     end
   end
 end

commit fb892157d76228ae9d4c96cf35ef27cb14cb9f42

Fix rails-only bug introduced in 26cffa593f5f7f5e669ed38ac1d5add3eb132c30.  Related to #1.
- 'rails_generator' can only be required if the rails environment is loaded, so defer requiring it
- Suppress generate task in rails (because it won't work there and you should use script/generate instead)

lib/shenandoah/rails/tasks.rb
@@ -1,13 +1,15 @@
-# This class is just sugar for initing Shenandoah::Tasks with a rails locator
-
 require 'shenandoah/tasks'
 require 'shenandoah/rails/locator'
 
 module Shenandoah
   module Rails
     class Tasks < Shenandoah::Tasks
+      def initialize(options = {})
+        super(options.merge(:generate_task => false))
+      end
+
       protected
-      
+
       def default_locator_type
         Locator
       end

lib/shenandoah/tasks.rb
@@ -2,8 +2,6 @@ require 'rake'
 require 'shenandoah/locator'
 require 'shenandoah/runner'
 require 'shenandoah/server'
-require 'rails_generator'
-require 'rails_generator/scripts/generate'
 
 module Shenandoah
   class Tasks
@@ -21,7 +19,7 @@ module Shenandoah
       create_serve_task
       create_shell_task
       create_run_task
-      create_gen_task
+      create_gen_task unless options[:generate_task] == false
     end
     
     def run_specs(pattern=nil)
@@ -43,6 +41,8 @@ module Shenandoah
     
     def generate_spec(name)
       raise "Please specify a spec name.  E.g., shen:generate[foo]." unless name
+      require 'rails_generator'
+      require 'rails_generator/scripts/generate'
       ::Rails::Generator::Base::prepend_sources( 
         ::Rails::Generator::PathSource.new(
           :shenandoah, File.join(File.dirname(__FILE__), '../../rails_generators'))